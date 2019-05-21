2379d2ce563c6171a7f6c1ec24ec46446f1dac09
Alex Livingstone <livingstone.alex@gmail.com>
2019-05-21 19:54:05 +1000

    add flask-admin for debugging

--- songmash.py
+++ songmash.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 import os
 import musicbrainzngs
 
@@ -25,5 +27,11 @@ musicbrainzngs.set_useragent("songmash", "0.1", "http://songmash.me")
 
 import views, models, utils
 
-if not os.path.exists(os.path.join(basedir, "app.db")):
-    db.create_all()
+if app.debug == True:
+    admin = Admin(app)
+    admin.add_view(ModelView(models.Artist, db.session))
+    admin.add_view(ModelView(models.Album, db.session))
+    admin.add_view(ModelView(models.Track, db.session))
+
+    if not os.path.exists(os.path.join(basedir, "app.db")):
+        db.create_all()
